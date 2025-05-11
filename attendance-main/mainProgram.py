6f44480e129dc80628b0adef203441e25dae22cf
eunjiiyu <rhizelleeunicedelacruz2611@gmail.com>
2025-05-11 12:38:02 +0800

    h

--- attendance-main/mainProgram.py
+++ attendance-main/mainProgram.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from PIL import ImageTk, Image
 from abc import ABC, abstractmethod
+from Login import LogInFrame
 
 def center_window(win, width, height):
         screen_width = win.winfo_screenwidth()
@@ -9,36 +10,25 @@ def center_window(win, width, height):
         y = (screen_height // 2) - (height // 2)
         win.geometry(f"{width}x{height}+{x}+{y}") 
 
-class Window(ABC):
+class Window:
     def __init__(self):
         self.root = ctk.CTk()
         self.root.title("Attendance Tracker")
-        center_window(self.root, 1000, 700)
+        center_window(self.root, 1280, 800)
+        self.root.configure(fg_color="#272757")
+        self.root.resizable(False, False)
 
     def run(self):
         self.root.mainloop()
 
-    @abstractmethod
-    def create_image(self):
-        pass
-    
-    @abstractmethod
-    def create_text(self):
-        pass
-
-
-class BasicUI(Window):
+class OpeningAnimation(Window):
     pass
 
 class LogIn(Window):
     def __init__(self):
         super().__init__()
-
-    def create_image(self):
-        pass
-
-    def create_text(self):
-        pass
+        self.login_ui = LogInFrame(self.root)
+        self.login_ui.place(rely=0.5, x=930 , anchor="center")
 
  
 if __name__ == "__main__":
